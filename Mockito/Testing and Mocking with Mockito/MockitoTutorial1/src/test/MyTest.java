980939c37cda06394f2edf4ffb8752c61e339c7b
Livre progamacao ltda-me <livre.programacao@gmail.com>
2016-04-12 09:53:57 -0300

    Using Mockito with JUnit.

---
+++ Mockito/Testing and Mocking with Mockito/MockitoTutorial1/src/test/MyTest.java
@@ -0,0 +1,25 @@
+import java.util.List;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import org.junit.Test;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MyTest {
+    
+    @Mock
+    private List<String> mockList;
+    
+    @Test
+    public void test() {
+        String expected = "Hello World!";
+        when(mockList.get(0)).thenReturn(expected);
+        
+        String actual = mockList.get(0);
+        assertEquals(expected, actual);
+        System.out.println(actual);
+    }
+    
+}
