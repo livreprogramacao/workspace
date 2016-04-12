82831e12b0bac3af820425d3401f22ad95c4dded
Livre progamacao ltda-me <livre.programacao@gmail.com>
2016-04-12 15:43:00 -0300

    Mockito Verification
    7. Download the Source Code

--- Mockito/mockito3-verification/src/main/java/com/javacodegeeks/hughwphamill/mockito/verification/Printer.java
+++ Mockito/mockito3-verification/src/main/java/com/javacodegeeks/hughwphamill/mockito/verification/Printer.java
@@ -1,11 +1,11 @@
 package com.javacodegeeks.hughwphamill.mockito.verification;
 
 public interface Printer {
-	
-	void printTestPage();
-	
-	void turnOff();
-	
-	void print(String text, Integer copies, Boolean collate);
+
+    void printTestPage();
+
+    void turnOff();
+
+    void print(String text, Integer copies, Boolean collate);
 
 }
--- Mockito/mockito3-verification/src/main/java/com/javacodegeeks/hughwphamill/mockito/verification/PrinterDiagnostics.java
+++ Mockito/mockito3-verification/src/main/java/com/javacodegeeks/hughwphamill/mockito/verification/PrinterDiagnostics.java
@@ -1,25 +1,25 @@
 package com.javacodegeeks.hughwphamill.mockito.verification;
 
 public class PrinterDiagnostics {
-	
-	private Printer printer;
-	
-	public PrinterDiagnostics(Printer printer) {
-		this.printer = printer;
-	}
-	
-	public void diagnosticPrint(String text, Integer copies, Boolean collate) {
-		StringBuilder diagnostic = new StringBuilder();
-		diagnostic.append("** Diagnostic Print **\n");
-		diagnostic.append("*** Copies: ").append(copies).append(" ***\n");
-		diagnostic.append("*** Collate: ").append(collate).append(" ***\n");
-		diagnostic.append("********************\n\n");
-		
-		printer.print(new StringBuilder().append(diagnostic).append(text).toString(), copies, collate);
-	}
-	
-	public void diagnosticAndOriginalPrint(String text, Integer copies, Boolean collate) {
-		diagnosticPrint(text, copies, collate);
-		printer.print(text, copies, collate);
-	}
+
+    private Printer printer;
+
+    public PrinterDiagnostics(Printer printer) {
+        this.printer = printer;
+    }
+
+    public void diagnosticPrint(String text, Integer copies, Boolean collate) {
+        StringBuilder diagnostic = new StringBuilder();
+        diagnostic.append("** Diagnostic Print **\n");
+        diagnostic.append("*** Copies: ").append(copies).append(" ***\n");
+        diagnostic.append("*** Collate: ").append(collate).append(" ***\n");
+        diagnostic.append("********************\n\n");
+
+        printer.print(new StringBuilder().append(diagnostic).append(text).toString(), copies, collate);
+    }
+
+    public void diagnosticAndOriginalPrint(String text, Integer copies, Boolean collate) {
+        diagnosticPrint(text, copies, collate);
+        printer.print(text, copies, collate);
+    }
 }
