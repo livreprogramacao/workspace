a9fea35a2af4cc94f0899811648c65f8b1d0161b
Livre progamacao ltda-me <livre.programacao@gmail.com>
2016-04-12 10:41:23 -0300

    In this tutorial we will take an in-depth look at stubbing classes and interfaces using Mockito.

--- Mockito/Testing and Mocking with Mockito/MockitoTutorial2/src/test/java/com/javacodegeeks/mockitotutorial2/processor/StringProcessorTest.java
+++ Mockito/Testing and Mocking with Mockito/MockitoTutorial2/src/test/java/com/javacodegeeks/mockitotutorial2/processor/StringProcessorTest.java
@@ -4,20 +4,26 @@ import com.javacodegeeks.mockitotutorial2.printer.Printer;
 import java.util.Optional;
 import static org.junit.Assert.assertFalse;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class StringProcessorTest {
 
-    private Printer printer;
+	@Mock
+	private Printer printer;
 
-    @Test
-    public void internal_buffer_should_be_absent_after_construction() {
-        // Given
-        StringProcessor processor = new StringProcessor(printer);
+	@Test
+	public void internal_buffer_should_be_absent_after_construction() {
+		// Given
+		StringProcessor processor = new StringProcessor(printer);
 
-        // When
-        Optional<String> actualBuffer = processor.statusAndTest();
+		// When
+		Optional<String> actualBuffer = processor.statusAndTest();
 
-        // Then
-        assertFalse(actualBuffer.isPresent());
-    }
+		// Then
+		assertFalse(actualBuffer.isPresent());
+	}
+
 }
