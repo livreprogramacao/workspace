cee46fbe1484e42597d7747f27d11f8bc1fed137
Livre progamacao ltda-me <livre.programacao@gmail.com>
2016-04-13 10:16:10 -0300

    JUnit - Parameterized Test
    Junit 4 has introduced a new feature Parameterized tests.Parameterized tests allow developer to run the same test over and over again using different values. There are five steps, that you need to follow to create Parameterized tests.
    
    Create a java class file name PrimeNumberCheckerTest.java
    
    Create Test Runner Class

--- JUnit/ParameterizedTest/ParameterizedTests/src/test/java/com/tutorialspoint/parameterizedtests/PrimeNumberCheckerTest.java
+++ JUnit/ParameterizedTest/ParameterizedTests/src/test/java/com/tutorialspoint/parameterizedtests/PrimeNumberCheckerTest.java
@@ -1,49 +1,51 @@
 package com.tutorialspoint.parameterizedtests;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import java.util.Arrays;
+import java.util.Collection;
 
-/**
- *
- * @author user
- */
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.runners.Parameterized;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
 public class PrimeNumberCheckerTest {
 
-    public PrimeNumberCheckerTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
+    private Integer inputNumber;
+    private Boolean expectedResult;
+    private PrimeNumberChecker primeNumberChecker;
 
     @Before
-    public void setUp() {
+    public void initialize() {
+        primeNumberChecker = new PrimeNumberChecker();
     }
 
-    @After
-    public void tearDown() {
+    // Each parameter should be placed as an argument here
+    // Every time runner triggers, it will pass the arguments
+    // from parameters we defined in primeNumbers() method
+    public PrimeNumberCheckerTest(Integer inputNumber,
+            Boolean expectedResult) {
+        this.inputNumber = inputNumber;
+        this.expectedResult = expectedResult;
     }
 
-    /**
-     * Test of validate method, of class PrimeNumberChecker.
-     */
+    @Parameterized.Parameters
+    public static Collection primeNumbers() {
+        return Arrays.asList(new Object[][]{
+            {2, true},
+            {6, false},
+            {19, true},
+            {22, false},
+            {23, true}
+        });
+    }
+
+    // This test will run 4 times since we have 5 parameters defined
     @Test
-    public void testValidate() {
-        System.out.println("validate");
-        Integer primeNumber = null;
-        PrimeNumberChecker instance = new PrimeNumberChecker();
-        Boolean expResult = null;
-        Boolean result = instance.validate(primeNumber);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testPrimeNumberChecker() {
+        System.out.println("Parameterized Number is : " + inputNumber);
+        assertEquals(expectedResult,
+                primeNumberChecker.validate(inputNumber));
     }
-
 }
