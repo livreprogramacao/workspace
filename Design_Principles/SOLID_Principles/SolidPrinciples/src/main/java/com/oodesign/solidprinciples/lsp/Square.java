235fee835c2d848cc76a502ffcf3390c16060d89
Livre progamacao ltda-me <livre.programacao@gmail.com>
2016-04-18 12:22:35 -0300

    SOLID Desing principles
    http://www.oodesign.com/liskov-s-substitution-principle.html

---
+++ Design_Principles/SOLID_Principles/SolidPrinciples/src/main/java/com/oodesign/solidprinciples/lsp/Square.java
@@ -0,0 +1,19 @@
+package com.oodesign.solidprinciples.lsp;
+
+/**
+ *
+ * @author user
+ */
+public class Square extends Rectangle {
+
+    public void setWidth(int width) {
+        m_width = width;
+        m_height = width;
+    }
+
+    public void setHeight(int height) {
+        m_width = height;
+        m_height = height;
+    }
+
+}
