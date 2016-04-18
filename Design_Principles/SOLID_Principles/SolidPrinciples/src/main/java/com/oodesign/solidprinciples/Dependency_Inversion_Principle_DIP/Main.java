703f5414e75fbad4afbfc5472d96bda2e3537e5c
Livre progamacao ltda-me <livre.programacao@gmail.com>
2016-04-18 11:32:34 -0300

    SOLID Desing principles
    http://www.oodesign.com/single-responsibility-principle.html

---
+++ Design_Principles/SOLID_Principles/SolidPrinciples/src/main/java/com/oodesign/solidprinciples/Dependency_Inversion_Principle_DIP/Main.java
@@ -0,0 +1,21 @@
+
+/**
+ * Single Responsibility Principle<
+ * <p>
+ * <strong>Motivation</strong>
+ * <p>
+ * In this context a responsibility is considered to be one reason to change. This principle states that if we have 2 reasons to change for a class, we have to split the functionality in two classes. Each class will handle only one responsibility and on future if we need to make one change we are going to make it in the class which handle it. When we need to make a change in a class having more responsibilities the change might affect the other functionality of the classes.
+ * <p>
+ * The Single Responsibility Principle is a simple and intuitive principle, but in practice it is sometimes hard to get it right.
+ * <p>
+ * <strong>Intent</strong>
+ * <p>
+ * A class should have only one reason to change.
+ * <p>
+ * @see <a href="http://www.oodesign.com/single-responsibility-principle.html">
+ * Single Responsibility Principle
+ * </a>
+ */
+public class Main {
+
+}
