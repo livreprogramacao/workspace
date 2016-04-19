4d979be0e9098887b7ca1b1c818ee6c466fc4baa
Livre progamacao ltda-me <livre.programacao@gmail.com>
2016-04-19 15:05:04 -0300

    Princípio da Responsabilidade Única (SRP)

---
+++ Object-Oriented Design/SOLID_Principles/srp/mauexemplo/pedido/Pedido.java
@@ -0,0 +1,8 @@
+package mauexemplo.pedido;
+
+public class Pedido
+{
+    public void AdicionarProduto(Produto produto, int quantidade) { }
+    public decimal CalcularTotal() { }
+    public void GerarPlanilhaExcel() { }
+}
