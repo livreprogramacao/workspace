ead8519c3c1a8ec5d423ad34d149876e79393bb4
Livre progamacao ltda-me <livre.programacao@gmail.com>
2016-04-19 16:12:58 -0300

    Princípios SOLID: Princípio da Segregação de Interface (ISP) | Robson Castilho

---
+++ Object-Oriented Design/SOLID_Principles/isp/MembroDeTimeScrum/src/main/java/br/com/robsoncastilho/membrodetimescrum/mauexemplo/MembroDeTimeScrum.java
@@ -0,0 +1,8 @@
+package br.com.robsoncastilho.membrodetimescrum.mauexemplo;
+
+public interface MembroDeTimeScrum
+{
+    void PriorizarBacklog();
+    void BlindarTime();
+    void ImplementarFuncionalidades();
+}
---
+++ Object-Oriented Design/SOLID_Principles/isp/MembroDeTimeScrum/src/main/java/br/com/robsoncastilho/membrodetimescrum/mauexemplo/ProductOwner.java
@@ -0,0 +1,21 @@
+package br.com.robsoncastilho.membrodetimescrum.mauexemplo;
+
+
+public class ProductOwner implements MembroDeTimeScrum {
+
+    @Override
+    public void PriorizarBacklog() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void BlindarTime() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void ImplementarFuncionalidades() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
