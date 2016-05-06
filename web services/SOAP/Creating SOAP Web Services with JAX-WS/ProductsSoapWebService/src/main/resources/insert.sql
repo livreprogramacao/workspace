ac4e4a7d529c98d3287f1f16853ef587aa0b7b9f
Livre progamacao ltda-me <livre.programacao@gmail.com>
2016-05-06 09:08:51 -0300

    Creating SOAP Web Services with JAX-WS.

--- web services/SOAP/Creating SOAP Web Services with JAX-WS/ProductsSoapWebService/src/main/resources/insert.sql
+++ web services/SOAP/Creating SOAP Web Services with JAX-WS/ProductsSoapWebService/src/main/resources/insert.sql
@@ -8,5 +8,13 @@
  * Author:  user
  * Created: May 6, 2016
  */
-INSERT INTO product_tbl (ID, NAME, COMPOSITION, TYPE, QUANTITY, COMPANY, ATTRIBUTE,PRICE) VALUES (1,'name','composition','type',1,'company','attribute',1.00)
-INSERT INTO product_tbl (ID, NAME, COMPOSITION, TYPE, QUANTITY, COMPANY, ATTRIBUTE,PRICE) VALUES (2, 'Levalon', 'Silymarin', 'TAB', 10, 'Brown & Burk', '70mg', 45.60)
+INSERT INTO product_tbl (ID, NAME, COMPOSITION, TYPE, QUANTITY, COMPANY, ATTRIBUTE,PRICE) VALUES (1,'Ultra Light Spread Tow Carbon Fiber','Carbon','Fiber',1,'Tow Carbon','Light',156.00)
+INSERT INTO product_tbl (ID, NAME, COMPOSITION, TYPE, QUANTITY, COMPANY, ATTRIBUTE,PRICE) VALUES (2, 'Levalon', 'Silymarin', 'TAB', 10, 'Brown & Burk', '181mg', 45.60)
+INSERT INTO product_tbl (ID, NAME, COMPOSITION, TYPE, QUANTITY, COMPANY, ATTRIBUTE,PRICE) VALUES (3, '1800 - reposado reserva', 'Silymarin', 'TAB', 10, 'Brown & Burk', '079mg', 45.60)
+INSERT INTO product_tbl (ID, NAME, COMPOSITION, TYPE, QUANTITY, COMPANY, ATTRIBUTE,PRICE) VALUES (4, '1800 - silver reserva', 'Silymarin', 'TAB', 10, 'Brown & Burk', '591mg', 45.60)
+INSERT INTO product_tbl (ID, NAME, COMPOSITION, TYPE, QUANTITY, COMPANY, ATTRIBUTE,PRICE) VALUES (5, '360 - double chocolate', 'Silymarin', 'TAB', 10, 'Brown & Burk', '55mg', 45.60)
+INSERT INTO product_tbl (ID, NAME, COMPOSITION, TYPE, QUANTITY, COMPANY, ATTRIBUTE,PRICE) VALUES (6, '360 - georgia peach', 'Silymarin', 'TAB', 10, 'Brown & Burk', '30mg', 45.60)
+INSERT INTO product_tbl (ID, NAME, COMPOSITION, TYPE, QUANTITY, COMPANY, ATTRIBUTE,PRICE) VALUES (7, 'CF10X', 'Silymarin', 'TAB', 10, 'Brown & Burk', '70mg', 45.60)
+INSERT INTO product_tbl (ID, NAME, COMPOSITION, TYPE, QUANTITY, COMPANY, ATTRIBUTE,PRICE) VALUES (8, 'CFT15X', 'Silymarin', 'TAB', 10, 'Brown & Burk', '70mg', 45.60)
+INSERT INTO product_tbl (ID, NAME, COMPOSITION, TYPE, QUANTITY, COMPANY, ATTRIBUTE,PRICE) VALUES (9, 'Gift Set With Hot Water Bottle, Mug & Hot Chocolate 25g', 'Silymarin', 'TAB', 10, 'Brown & Burk', '70mg', 45.60)
+INSERT INTO product_tbl (ID, NAME, COMPOSITION, TYPE, QUANTITY, COMPANY, ATTRIBUTE,PRICE) VALUES (10, 'Adult Colouring Books, A3 Size', 'Silymarin', 'TAB', 10, 'Brown & Burk', '70mg', 45.60)
