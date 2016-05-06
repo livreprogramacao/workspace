b07d5487d2c2be9798e3d3a08ff36d7c0d3d9f96
Livre progamacao ltda-me <livre.programacao@gmail.com>
2016-05-06 08:26:26 -0300

    Creating SOAP Web Services with JAX-WS.

---
+++ web services/SOAP/Creating SOAP Web Services with JAX-WS/ProductsSoapWebService/src/main/resources/insert.sql
@@ -0,0 +1,11 @@
+/*
+*   Livre Programação ltda-ME
+*
+*   Criando um Web Services SOAP com JAX-WS.
+*
+*/
+/**
+ * Author:  user
+ * Created: May 6, 2016
+ */
+insert product (id,name,composition,type,quantity,company,attribute,price) values (1,'name','composition','type',1,'company','attribute',1.00)
