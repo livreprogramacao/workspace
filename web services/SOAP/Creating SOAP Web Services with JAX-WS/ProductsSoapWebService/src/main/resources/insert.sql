ffe90e047eae8ef14c6c0348755ea0c558a0494d
Livre progamacao ltda-me <livre.programacao@gmail.com>
2016-05-06 08:52:39 -0300

    Creating Web Service Consumer.Creating SOAP Web Services with JAX-WS.

--- web services/SOAP/Creating SOAP Web Services with JAX-WS/ProductsSoapWebService/src/main/resources/insert.sql
+++ web services/SOAP/Creating SOAP Web Services with JAX-WS/ProductsSoapWebService/src/main/resources/insert.sql
@@ -9,4 +9,4 @@
  * Created: May 6, 2016
  */
 INSERT INTO product_tbl (ID, NAME, COMPOSITION, TYPE, QUANTITY, COMPANY, ATTRIBUTE,PRICE) VALUES (1,'name','composition','type',1,'company','attribute',1.00)
-INSERT INTO product_tbl (ID, NAME, COMPOSITION, TYPE, QUANTITY, COMPANY, ATTRIBUTE,PRICE) VALUES (2, 'Levalon', 'Silymarin', 'TAB', 10, 'Brown & Burk', '70mg', 45.60);
+INSERT INTO product_tbl (ID, NAME, COMPOSITION, TYPE, QUANTITY, COMPANY, ATTRIBUTE,PRICE) VALUES (2, 'Levalon', 'Silymarin', 'TAB', 10, 'Brown & Burk', '70mg', 45.60)
